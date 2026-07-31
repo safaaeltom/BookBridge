674c1b2ee44476f6630b63d04f669bdaa8995c7d
Safaa <safaaeltom@gmail.com>
2026-07-31 12:22:42 +0300

    connect country context provider to app

--- BookBridgeProject/src/main.jsx
+++ BookBridgeProject/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { CountryProvider } from './Context/CountryContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <CountryProvider>
+        <App />
+      </CountryProvider>
     </BrowserRouter>
   </StrictMode>,
 )
